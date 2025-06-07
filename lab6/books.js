a3fe82114d035aaed9491ece56248f3320a88b3e
Полина Селихова <pselihova3@gmail.com>
2025-06-08 02:25:12 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,73 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name:'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear:2022
+    },
+    {
+        author: 'Карл Саган',
+        name:'Космос',
+        publicationYear: 2024
+    },
+    {
+        author: 'Баладжи Касал',
+        name:'Ошибки разумного инвестора',
+        publicationYear: 2025
+    },
+     {
+        author: 'Дмитрий Мордас',
+        name: 'Зайчик',
+        publicationYear: 2025,
+        price: 450,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Влад Райбер',
+        name: 'Летний лагерь',
+        publicationYear: 2025,
+        price: 399,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Собачье сердце',
+        publicationYear: 1925,
+        price: 350,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Александр Пушкин',
+        name: 'Капитанская дочка',
+        publicationYear: 1836,
+        price: 290,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Михаил Шолохов',
+        name: 'Судьба человека',
+        publicationYear: 1956,
+        price: 420,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Михаил Лермонтов',
+        name: 'Герой нашего времени',
+        publicationYear: 1840,
+        price: 380,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Николай Гоголь',
+        name: 'Шинель',
+        publicationYear: 1842,
+        price: 310,
+        bestseller: false,
+        available: true
+    },
+]
